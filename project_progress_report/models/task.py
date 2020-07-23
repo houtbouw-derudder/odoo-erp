7dfc6bd8c6af36448a81383bed5ed97e7753e290
Dries De Rudder <dries.derudder@scaledaccess.com>
2020-07-23 11:23:05 +0200

    indentation

--- project_progress_report/models/task.py
+++ project_progress_report/models/task.py
@@ -8,7 +8,7 @@ class Task(models.Model):
 	sale_price = fields.Float('Price', digits='Product Price')
 
 	@api.depends('company_id')
-    def _compute_sale_currency_id(self):
-        main_company = self.env['res.company']._get_main_company()
-        for task in self:
-            task.sale_currency_id = task.company_id.sudo().currency_id.id or main_company.currency_id.id
+	def _compute_sale_currency_id(self):
+		main_company = self.env['res.company']._get_main_company()
+		for task in self:
+			task.sale_currency_id = task.company_id.sudo().currency_id.id or main_company.currency_id.id
