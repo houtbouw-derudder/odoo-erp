db4f5bb390e25ccef8498e6f456f9d9cb1348393
Dries De Rudder <dries.derudder@scaledaccess.com>
2020-07-23 11:04:03 +0200

    model taks sale_currency_id & sale_price

---
+++ project_progress_report/models/taks.py
@@ -0,0 +1,14 @@
+
+from odoo import models, fields
+
+class Task(models.Model):
+	_inherit: 'project.task'
+
+	sale_currency_id = fields.Many2one('res.currency', 'Currency', compute='_compute_sale_currency_id')
+	sale_price = fields.Float('Price', digits='Product Price')
+
+	@api.depends('company_id')
+    def _compute_sale_currency_id(self):
+        main_company = self.env['res.company']._get_main_company()
+        for task in self:
+            task.sale_currency_id = task.company_id.sudo().currency_id.id or main_company.currency_id.id
