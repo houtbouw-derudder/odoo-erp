23eae73d476482ca2b8a9e005f5fd90cf22575fa
Dries De Rudder <dries.derudder@scaledaccess.com>
2020-07-23 15:55:22 +0200

    char field

--- project_progress_report/models/task.py
+++ project_progress_report/models/task.py
@@ -6,7 +6,7 @@ class Task(models.Model):
 	_description = "Task extension for progress report"
 
 	sale_quantity = fields.Float('Quantity', digits='1.3f', tracking=True, default=1.0)
-	sale_unit = fields.String('Unit', tracking=True, default='sog')
+	sale_unit = fields.Char('Unit', tracking=True, default='sog')
 	sale_currency_id = fields.Many2one('res.currency', 'Currency', compute='_compute_sale_currency_id', tracking=True)
 	sale_unit_price = fields.Float('Unit price', digits='Product Price', tracking=True)
 
