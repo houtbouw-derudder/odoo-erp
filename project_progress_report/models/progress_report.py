91bfc800bed98b1e3151b1cbcf3b2b38e881850a
Dries De Rudder <dries.derudder@scaledaccess.com>
2020-07-24 08:41:17 +0200

    add class TaskProgress

--- project_progress_report/models/progress_report.py
+++ project_progress_report/models/progress_report.py
@@ -5,3 +5,6 @@ from odoo import models, fields, api
 class ProgressReport(models.Model):
 	_name = 'project.progress.report'
 	_description = 'A Progress Report for tasks in a project'
+
+	project_id = fields.Many2one('project.project', string='Project', default=lambda self: self.env.context.get('default_project_id'),
+        index=True, tracking=True, check_company=True)
---
+++ project_progress_report/models/task_progress.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class TaskProgress(models.Model):
+	_name = 'project.task.progress'
+	_description = 'The progress of a task at the time the progress report was generated'
+
+	progress_report_id = fields.Many2one('project.progress.report', string='Progress report', default=lambda self: self.env.context.get('default_progress_report_id'),
+        index=True, tracking=True)
+		
