74338bc63e491df327f9fa4d728e86b5af09ffc2
Dries De Rudder <dries.derudder@scaledaccess.com>
2020-07-24 15:16:14 +0200

    add security

--- project_progress_report/__manifest__.py
+++ project_progress_report/__manifest__.py
@@ -20,7 +20,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         # 'templates.xml',
         'data/progress_report_data.xml',
         'views/task_views.xml',
