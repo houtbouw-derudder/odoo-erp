9fc6bbd95adb9ff11b1e8277208f53ab96eee26a
Dries De Rudder <dries.derudder@scaledaccess.com>
2020-07-24 15:10:46 +0200

    progress report sequence

--- project_progress_report/models/progress_report.py
+++ project_progress_report/models/progress_report.py
@@ -6,5 +6,6 @@ class ProgressReport(models.Model):
 	_name = 'project.progress.report'
 	_description = 'A Progress Report for tasks in a project'
 
+	name = fields.Char(string="Name", required=True, default='Draft')
 	project_id = fields.Many2one('project.project', string='Project', default=lambda self: self.env.context.get('default_project_id'),
         index=True, tracking=True, check_company=True, required=True)
