567aff6bb98301909d6ea8c4cf1eff2b363ab2cd
Dries De Rudder <dries.derudder@scaledaccess.com>
2020-07-24 15:49:51 +0200

    add name,state,date on progress report

--- project_progress_report/models/progress_report.py
+++ project_progress_report/models/progress_report.py
@@ -1,11 +1,33 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+
 
 class ProgressReport(models.Model):
-	_name = 'project.progress.report'
-	_description = 'A Progress Report for tasks in a project'
+    _name = 'project.progress.report'
+    _description = 'A Progress Report for tasks in a project'
 
-	name = fields.Char(string="Name", required=True, default='Draft')
-	project_id = fields.Many2one('project.project', string='Project', default=lambda self: self.env.context.get('default_project_id'),
-        index=True, tracking=True, check_company=True, required=True)
+    name = fields.Char(string='Number', required=True,
+                       readonly=True, copy=False, default='/')
+    date = fields.Date(string='Date', required=True, index=True, readonly=True,
+                       states={'draft': [('readonly', False)]},
+                       default=fields.Date.context_today)
+    project_id = fields.Many2one('project.project', string='Project', default=lambda self: self.env.context.get('default_project_id'),
+                                 index=True, tracking=True, check_company=True, required=True, domain=[[]])
+    state = fields.Selection(selection=[
+        ('draft', 'Draft'),
+        ('approved', 'Approved'),
+        ('cancel', 'Cancelled')
+    ], string='Status', required=True, readonly=True, copy=False, tracking=True,
+        default='draft')
+
+    @api.depends('name', 'state')
+    def name_get(self):
+        result = []
+        for report in self:
+            if report.state == 'draft':
+                name = _("Draft")
+            else:
+                name = report.name
+            result.append((report.id, name))
+        return result
