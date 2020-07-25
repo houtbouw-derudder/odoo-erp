6b26d501bfbf7a6598b218c119ee1514bcbab693
Dries De Rudder <dries.derudder@scaledaccess.com>
2020-07-25 14:23:27 +0200

    correct field name

--- project_progress_report/models/task_progress.py
+++ project_progress_report/models/task_progress.py
@@ -7,7 +7,7 @@ class TaskProgress(models.Model):
     _description = 'The progress of a task at the time the progress report was generated'
 
     progress_report_id = fields.Many2one('project.progress.report', string='Progress report', readonly=True)
-    task_id = fields.Many2one('project.task', string="Project", readonly=True)
+    task_id = fields.Many2one('project.task', string="Task", readonly=True)
     progress_quantity = fields.Float('Quantity', readonly=True)
     progress_unit = fields.Char('Unit', readonly=True)
     progress_currency_id = fields.Many2one('res.currency', 'Currency', readonly=True)
