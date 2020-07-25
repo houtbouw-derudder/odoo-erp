3ebb84d50b0001b675857f86ecc4a535bfec6ac0
Dries De Rudder <dries.derudder@scaledaccess.com>
2020-07-25 14:25:43 +0200

    add progress_price

--- project_progress_report/models/task_progress.py
+++ project_progress_report/models/task_progress.py
@@ -2,16 +2,25 @@
 
 from odoo import models, fields, api
 
+
 class TaskProgress(models.Model):
     _name = 'project.task.progress'
     _description = 'The progress of a task at the time the progress report was generated'
 
-    progress_report_id = fields.Many2one('project.progress.report', string='Progress report', readonly=True)
+    progress_report_id = fields.Many2one(
+        'project.progress.report', string='Progress report', readonly=True)
     task_id = fields.Many2one('project.task', string="Task", readonly=True)
     progress_quantity = fields.Float('Quantity', readonly=True)
     progress_unit = fields.Char('Unit', readonly=True)
-    progress_currency_id = fields.Many2one('res.currency', 'Currency', readonly=True)
+    progress_currency_id = fields.Many2one(
+        'res.currency', 'Currency', readonly=True)
     progress_unit_price = fields.Float('Unit price', readonly=True)
     progress_total_price = fields.Float('Total price', readonly=True)
     progress_percentage = fields.Float("Progress precentage", readonly=True)
-    
+
+    def _compute_progress_price(self):
+        for task_progress in self:
+            task_progress.progress_price = task_progress.progress_total_price * task_progress.progress_percentage
+
+    progress_price = fields.Float(
+        'Progress price', readonly=True, compute='_compute_progress_price', store=True)
