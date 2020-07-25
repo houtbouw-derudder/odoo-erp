4746ff114fff82a2137909e4a54b068fc8e816fe
Dries De Rudder <dries.derudder@scaledaccess.com>
2020-07-25 10:13:20 +0200

    make task_progress readonly

--- project_progress_report/models/task_progress.py
+++ project_progress_report/models/task_progress.py
@@ -6,12 +6,12 @@ class TaskProgress(models.Model):
     _name = 'project.task.progress'
     _description = 'The progress of a task at the time the progress report was generated'
 
-    progress_report_id = fields.Many2one('project.progress.report', string='Progress report')
-    task_id = fields.Many2one('project.task', string="Project")
-    progress_quantity = fields.Float('Quantity')
-    progress_unit = fields.Char('Unit')
-    progress_currency_id = fields.Many2one('res.currency', 'Currency')
-    progress_unit_price = fields.Float('Unit price')
-    progress_total_price = fields.Float('Total price')
-    progress_percentage = fields.Float("Progress precentage")
+    progress_report_id = fields.Many2one('project.progress.report', string='Progress report', readonly=True)
+    task_id = fields.Many2one('project.task', string="Project", readonly=True)
+    progress_quantity = fields.Float('Quantity', readonly=True)
+    progress_unit = fields.Char('Unit', readonly=True)
+    progress_currency_id = fields.Many2one('res.currency', 'Currency', readonly=True)
+    progress_unit_price = fields.Float('Unit price', readonly=True)
+    progress_total_price = fields.Float('Total price', readonly=True)
+    progress_percentage = fields.Float("Progress precentage", readonly=True)
     
