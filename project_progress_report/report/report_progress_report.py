c5ad3c6efdf5de64a3f746c616cc8f91ee7cbc80
Dries De Rudder <dries.derudder@scaledaccess.com>
2020-07-29 17:59:51 +0200

    start implementing _get_report_values

--- project_progress_report/report/report_progress_report.py
+++ project_progress_report/report/report_progress_report.py
@@ -13,20 +13,25 @@ class ReportProgressReport(models.AbstractModel):
     _name = 'report.project_progress_report.report_progress_report'
     _description = "Project progress report"
 
-    def _get_report_values(self, docids, data=None):
-        _logger.warning(docids)
-        _logger.warning(data)
-        
+    def _get_report_values(self, docids, data=None):        
         report_obj = self.env['ir.actions.report']
-        _logger.warning(report_obj)
         report = report_obj._get_report_from_name('project_progress_report.report_progress_report')
-        _logger.warning(report)
 
-        docs = self.env['project.progress.report'].browse(docids)
+        progress_reports = self.env['project.progress.report'].browse(docids)
+
+        data = []
+        for progress_report in progress_reports:
+            val = {}
+            val['name'] = progress_report.name
+            val['date'] = progress_report.date
+            val['project_id'] = progress_report.project_id
+            val['previous_progress_report_id'] = progress_report.previous_progress_report_id
+            val['task_progress_ids'] = progress_report.task_progress_ids
+            data.append(val)
 
         docargs = {
             'doc_ids': docids,
             'doc_model': report.model,
-            'docs': docs,
+            'docs': data,
         }
         return docargs
