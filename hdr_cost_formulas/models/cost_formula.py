b3073294f3b1159b03e41e63da30fea42abbdad4
Dries De Rudder <dries.derudder@gmail.com>
2020-12-22 17:07:21 +0100

    mail.thread

--- hdr_cost_formulas/models/cost_formula.py
+++ hdr_cost_formulas/models/cost_formula.py
@@ -6,14 +6,15 @@ class CostFormula(models.Model):
 
     _name = "cost.formula"
     _description = "Cost formula"
+    _inherit = ['mail.thread']
     _order = 'sequence, id'
 
     name = fields.Char(string='Name', required=True, translate=True)
     description = fields.Text(string='Description', translate=True)
     sequence = fields.Integer(default=1)
 
-    parameters = fields.Char(string="Parameters", help="Comma separated list of parameter names")
-    view = fields.Text(string='HTML + JavaScript')
+    parameters = fields.Char(string="Parameters", help="Comma separated list of parameter names", tracking=True)
+    view = fields.Text(string='HTML + JavaScript', tracking=True)
 
 class CostItem(models.Model):
     _name = "cost.item"
