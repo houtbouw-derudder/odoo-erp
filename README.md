db571dd5cbe42e76dda5086bc3bc0c8aa48302a5
Dries De Rudder <dries.derudder@gmail.com>
2020-12-22 10:00:21 +0100

    initial setup of odoo module

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # odoo-erp
+
+use Python 3.8
+create venv
+
+pip install -r ./requirements-dev.txt
---
+++ hdr_cost_formulas/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Cost fomrulas",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Houtbouw De Rudder bv",
+    'category': 'Houtbouw De Rudder',
+    'description': """
+    Manage cost formulas
+    """,
+    # data files always loaded at installation
+    'data': [
+        # 'views/mymodule_view.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        # 'demo/demo_data.xml',
+    ],
+}
