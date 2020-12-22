e4788aca8388d898a3dd608f9156c95614ef05ff
Dries De Rudder <dries.derudder@gmail.com>
2020-12-22 16:02:16 +0100

    add parameters & view attributes to cost formula

--- hdr_cost_formulas/models/cost_formula.py
+++ hdr_cost_formulas/models/cost_formula.py
@@ -11,3 +11,13 @@ class CostFormula(models.Model):
     name = fields.Char(string='Name', required=True, translate=True)
     description = fields.Text(string='Description', translate=True)
     sequence = fields.Integer(default=1)
+
+    parameters = fields.Char(string="Parameters", help="Comma separated list of parameter names")
+    view = fields.Text(string='HTML + JavaScript')
+
+class CostItem(models.Model):
+    _name = "cost.item"
+    _description = "Cost item"
+    _order = 'sequence, id'
+
+    sequence = fields.Integer(default=1)
