97640a1a10fa24b93c9f108b9ebe6d11f9acb716
Dries De Rudder <dries.derudder@gmail.com>
2021-03-23 09:50:04 +0100

    initial module setup

---
+++ project_customization/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Project Customization",
+
+    'summary': """Customizaties voor Houtbouw De Rudder""",
+
+    'description': "",
+
+    'author': "Houtbouw De Rudder bv",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Services/Project',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['project'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        # 'demo.xml',
+    ],
+}
