c5064d59f427c078ad446b00dc1068306d992a62
Dries De Rudder <dries.derudder@gmail.com>
2021-08-04 11:15:25 +0200

    fix hdr.planning.capability model

--- hdr_project_planning/models/planning_capability.py
+++ hdr_project_planning/models/planning_capability.py
@@ -3,6 +3,7 @@ from odoo import fields, models, api
 
 class Capability(models.Model):
     _name = 'hdr.planning.capability'
+    _description = 'A capability to assing to a resource in orde to make a resource capacity planning'
 
-    name = fields.Char(string='Name',required=True,index=True,tracking=True)
+    name = fields.Char(string='Name',required=True,index=True)
     description = fields.Char(string='Description')
