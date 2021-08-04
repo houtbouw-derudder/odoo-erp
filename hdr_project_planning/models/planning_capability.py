b7c64efe9f897c14fbdd7642d0216f01d122d4d9
Dries De Rudder <dries.derudder@gmail.com>
2021-08-04 10:34:55 +0200

    config menu & capabilities tree

--- hdr_project_planning/models/planning_capability.py
+++ hdr_project_planning/models/planning_capability.py
@@ -2,5 +2,7 @@
 from odoo import fields, models, api
 
 class Capability(models.Model):
+    _name = 'hdr.planning.capability'
+
     name = fields.Char(string='Name',required=True,index=True,tracking=True)
     description = fields.Char(string='Description')
