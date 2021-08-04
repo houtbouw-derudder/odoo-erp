5bfbc91d5265161e1fed0271548afae70129b8af
Dries De Rudder <dries.derudder@gmail.com>
2021-08-04 12:37:44 +0200

    fix typo

--- hdr_project_planning/models/planning_capability.py
+++ hdr_project_planning/models/planning_capability.py
@@ -3,7 +3,7 @@ from odoo import fields, models, api
 
 class Capability(models.Model):
     _name = 'hdr.planning.capability'
-    _description = 'A capability to assing to a resource in orde to make a resource capacity planning'
+    _description = 'A capability to assing to a resource in order to make a resource capacity planning'
 
     name = fields.Char(string='Name',required=True,index=True)
     description = fields.Char(string='Description')
