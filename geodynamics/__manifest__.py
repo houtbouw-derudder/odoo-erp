821a92c6aea54c09732aa52b32a8c171eae2a454
Dries De Rudder <dries.derudder@gmail.com>
2022-01-07 12:17:53 +0100

    initial setup

---
+++ geodynamics/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Geodynamics',
+    'version': '1.0',
+    'category': 'Customization',
+    'author': 'Houtbouw De Rudder bv',
+    'description': """
+        Integration with geodynamics
+    """,
+    'depends': [
+        'project',
+        'hr_timesheet'
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'OPL-1'
+}
