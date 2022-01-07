9157256667bf8c581c50868703a2afc218f943bb
Dries De Rudder <dries.derudder@gmail.com>
2022-01-07 23:45:33 +0100

    create all at once

--- geodynamics/models/geodynamics_postcalculation.py
+++ geodynamics/models/geodynamics_postcalculation.py
@@ -1,16 +1,20 @@
 
 from odoo import api, fields, models, _, tools
 
+
 class GeodynamicsPostCalculationLine(models.Model):
     _name = 'geodynamics.postcalculation.line'
     _description = 'Geodynamics Postcalculation line'
-    
-    postcalculation_id = fields.Many2one('geodynamics.postcalculation', 'Postcalculation', required=True, ondelete='restrict', index=True)
-    date = fields.Date(compute='_compute_date',store=True)
+
+    postcalculation_id = fields.Many2one(
+        'geodynamics.postcalculation', 'Postcalculation', required=True, ondelete='restrict', index=True)
+    date = fields.Date(compute='_compute_date', store=True)
     employee_external_id = fields.Char(required=True)
-    employee_id = fields.Many2one('hr.employee', 'Employee', compute='_compute_employee', store=True)
+    employee_id = fields.Many2one(
+        'hr.employee', 'Employee', compute='_compute_employee', store=True)
     task_external_id = fields.Char(required=True)
-    task_id = fields.Many2one('project.task', 'Task', compute='_compute_task', store=True)
+    task_id = fields.Many2one('project.task', 'Task',
+                              compute='_compute_task', store=True)
     duration = fields.Float(default=0.0)
     km_home_work = fields.Float(default=0.0)
     km_driver = fields.Float(default=0.0)
@@ -21,20 +25,24 @@ class GeodynamicsPostCalculationLine(models.Model):
     def _compute_date(self):
         for record in self:
             record.date = record.postcalculation_id.date
-    
+
     @api.depends('task_external_id')
     def _compute_task(self):
         for record in self:
             if record.task_external_id:
-                task_id_from_external = self.env.ref(record.task_external_id).id
-                record.task_id = self.env['project.task'].search([('id', '=', task_id_from_external)], limit=1)
+                task_id_from_external = self.env.ref(
+                    record.task_external_id).id
+                record.task_id = self.env['project.task'].search(
+                    [('id', '=', task_id_from_external)], limit=1)
 
     @api.depends('employee_external_id')
     def _compute_employee(self):
         for record in self:
             if record.employee_external_id:
-                employee_id_from_external = self.env.ref(record.employee_external_id).id
-                record.employee_id = self.env['hr.employee'].search([('id', '=', employee_id_from_external)], limit=1)
+                employee_id_from_external = self.env.ref(
+                    record.employee_external_id).id
+                record.employee_id = self.env['hr.employee'].search(
+                    [('id', '=', employee_id_from_external)], limit=1)
 
     # datum = pc['Date'].split('T')[0]
     #     werknemer = pc['User']['Name']
@@ -55,13 +63,23 @@ class GeodynamicsPostCalculation(models.Model):
     _rec_name = 'date'
 
     date = fields.Date(required=True, default=fields.Date.context_today)
-    state = fields.Selection(selection=[('draft','Draft'),('validated','Validated')], default='draft')
+    state = fields.Selection(
+        selection=[('draft', 'Draft'), ('validated', 'Validated')], default='draft')
 
-    line_ids = fields.One2many('geodynamics.postcalculation.line', 'postcalculation_id', string="Postcalculation Lines")
+    line_ids = fields.One2many('geodynamics.postcalculation.line',
+                               'postcalculation_id', string="Postcalculation Lines")
 
     _sql_constraints = [
-        ('date_unique', 'unique (date)', 'The date of the postcalculation must be unique !')
+        ('date_unique', 'unique (date)',
+         'The date of the postcalculation must be unique !')
     ]
+
+    def _extract_postcalculation_line_data(self, data):
+        return {
+            'postcalculation_id': self.id,
+            'employee_external_id': data['User']['Code'],
+            'task_external_id': data['PostCalculation']['CostCenter']
+        }
 
     def action_reload(self):
         self.ensure_one()
@@ -72,14 +90,9 @@ class GeodynamicsPostCalculation(models.Model):
 
         api = self.env['geodynamics.api']
         postcalculation_data = api.load_postcalculation(self.date)
-        for pc in postcalculation_data:
-            vals = {
-                'postcalculation_id': self.id,
-                'employee_external_id': pc['User']['Code'],
-                'task_external_id': pc['PostCalculation']['CostCenter']
-            }
 
-            self.line_ids += self.env['geodynamics.postcalculation.line'].create(vals)
+        self.line_ids = self.env['geodynamics.postcalculation.line'].create(
+            [self._extract_postcalculation_line_data(pc) for pc in postcalculation_data])
 
     def action_validate(self):
         self.ensure_one()
