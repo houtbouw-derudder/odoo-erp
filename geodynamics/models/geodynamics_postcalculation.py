ba909d54d07e443b3db987c76230c76d531a6fe5
Dries De Rudder <dries.derudder@gmail.com>
2022-01-07 23:41:39 +0100

    add check for value

--- geodynamics/models/geodynamics_postcalculation.py
+++ geodynamics/models/geodynamics_postcalculation.py
@@ -25,14 +25,16 @@ class GeodynamicsPostCalculationLine(models.Model):
     @api.depends('task_external_id')
     def _compute_task(self):
         for record in self:
-            task_id_from_external = self.env.ref(record.task_external_id).id
-            record.task_id = self.env['project.task'].search([('id', '=', task_id_from_external)], limit=1)
+            if record.task_external_id:
+                task_id_from_external = self.env.ref(record.task_external_id).id
+                record.task_id = self.env['project.task'].search([('id', '=', task_id_from_external)], limit=1)
 
     @api.depends('employee_external_id')
     def _compute_employee(self):
         for record in self:
-            employee_id_from_external = self.env.ref(record.employee_external_id).id
-            record.employee_id = self.env['hr.employee'].search([('id', '=', employee_id_from_external)], limit=1)
+            if record.employee_external_id:
+                employee_id_from_external = self.env.ref(record.employee_external_id).id
+                record.employee_id = self.env['hr.employee'].search([('id', '=', employee_id_from_external)], limit=1)
 
     # datum = pc['Date'].split('T')[0]
     #     werknemer = pc['User']['Name']
