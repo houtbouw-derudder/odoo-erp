df97e108a214512ad0479ba04da75895a3d3d9e4
Dries De Rudder <dries.derudder@gmail.com>
2022-01-09 18:35:31 +0100

    setup module project_customization

---
+++ project_customization/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Project - Customization',
+    'version': '1.0',
+    'category': 'Customization',
+    'author': 'Houtbouw De Rudder bv',
+    'description': """
+        Customization to project app
+    """,
+    'depends': [
+        'project'
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+    'license': 'OPL-1'
+}
