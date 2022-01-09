5cc742ef9ececa736ef36caa88a5d80bdce66cf1
Dries De Rudder <dries.derudder@gmail.com>
2022-01-09 08:36:12 +0100

    rename company_name

--- geodynamics/models/res_config_settings.py
+++ geodynamics/models/res_config_settings.py
@@ -6,6 +6,6 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     api_basic_auth_header = fields.Char(config_parameter='geodynamics.api_basic_auth_header', string="Basic Auth header")
-    company_name = fields.Char(config_parameter='geodynamics.company_name', string="Company name")
+    geo_company_name = fields.Char(config_parameter='geodynamics.company_name', string="Company name")
     user_name = fields.Char(config_parameter='geodynamics.user_name',string='User name')
     password = fields.Char(config_parameter='geodynamics.password',string='Password')
