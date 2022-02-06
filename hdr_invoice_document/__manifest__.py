de97b984d25bcbbda83f0fc749ceccd64690a42d
Dries De Rudder <dries.derudder@gmail.com>
2022-02-06 09:10:13 +0100

    initial setup

---
+++ hdr_invoice_document/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'HDR Invoice document',
+    'version': '1.0',
+    'category': 'Accounting/Localizations',
+    'author': 'Houtbouw De Rudder bv',
+    'description': """
+        Custom Invoice Document
+    """,
+    'depends': [
+        'account'
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'license': 'OPL-1'
+}
