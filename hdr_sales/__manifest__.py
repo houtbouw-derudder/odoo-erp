70699493c96dec89c441bd68a469b2567336323e
Dries De Rudder <dries.derudder@gmail.com>
2022-03-21 12:37:17 +0100

    initial sales module

---
+++ hdr_sales/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'HDR Sales',
+    'version': '0.1',
+    'category': 'Sales',
+    'author': 'Houtbouw De Rudder bv',
+    'description': """
+        Sales
+    """,
+    'depends': [
+        'base'
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'license': 'OPL-1'
+}
