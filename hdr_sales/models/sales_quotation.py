8be881206376f706292c43c3f10ec8a78c975fbf
Dries De Rudder <dries.derudder@gmail.com>
2022-03-21 14:17:09 +0100

    fields on quotation

--- hdr_sales/models/sales_quotation.py
+++ hdr_sales/models/sales_quotation.py
@@ -6,8 +6,41 @@ class Quotation(models.Model):
     _name = 'hdr_sales.quotation'
     _description = 'Sales quotation'
 
-    name = fields.Char(string="Name", required=True)
-    partner_id = fields.Many2one('res.partner', 'Partner')
-    date = fields.Date(required=True, default=fields.Date.context_today)
-    state = fields.Selection(
-        selection=[('draft', 'Draft'), ('validated', 'Validated')], default='draft')
+    name = fields.Char(string='Number', copy=False,
+                       readonly=False, store=True, index=True, tracking=True)
+    date = fields.Date(
+        string='Date',
+        required=True,
+        index=True,
+        readonly=True,
+        states={'draft': [('readonly', False)]},
+        copy=False,
+        tracking=True,
+        default=fields.Date.context_today
+    )
+    ref = fields.Char(string='Reference', copy=False, tracking=True)
+
+    state = fields.Selection(selection=[
+        ('draft', 'Draft'),
+        ('posted', 'Posted'),
+        ('cancel', 'Cancelled'),
+    ], string='Status', required=True, readonly=True, copy=False, tracking=True,
+        default='draft')
+
+    document_type = fields.Selection(selection=[
+        ('quotation', 'Quotation'),
+        ('estimate', 'Estimate'),
+    ], string='Type', required=True, store=True, index=True, readonly=True, tracking=True,
+        default="entry", change_default=True)
+
+    company_id = fields.Many2one(comodel_name='res.company', string='Company',
+                                 store=True, readonly=True,
+                                 default=lambda self: self.env.company)
+
+    company_currency_id = fields.Many2one(string='Company Currency', readonly=True,
+                                          related='company_id.currency_id')
+
+    partner_id = fields.Many2one('res.partner', readonly=True, tracking=True,
+                                 states={'draft': [('readonly', False)]},
+                                 check_company=True,
+                                 string='Partner', change_default=True)
