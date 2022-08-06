b70eeafd5e3adb644e57652fb11a5791fc54a652
Dries De Rudder <dries.derudder@gmail.com>
2022-08-06 13:51:20 +0200

    add purchase item model

---
+++ hdr_aankoop/models/purchase_item.py
@@ -0,0 +1,15 @@
+
+from odoo import fields, models, api
+
+class ProjectTask(models.Model):
+    _name = 'hdr.purchase.item'
+    _description = "Aan te kopen item voor een bepaald project"
+    _inherit = ['mail.thread']
+
+    name = fields.Char(required=True)
+    date = fields.Date(required=True, default=fields.Date.context_today)
+    project_id = fields.Many2one('project.project', 'Project')
+    supplier_id = fields.Many2one('res.partner', 'Supplier')
+    description = fields.Html()
+    quantity = fields.Char()
+    budget = fields.Char()
