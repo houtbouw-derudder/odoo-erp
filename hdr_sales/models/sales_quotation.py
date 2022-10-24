3e675401d98d6aabd068c727088965b11a3b8cce
Dries De Rudder <dries.derudder@gmail.com>
2022-10-24 15:22:03 +0200

    total_amount_untaxed

--- hdr_sales/models/sales_quotation.py
+++ hdr_sales/models/sales_quotation.py
@@ -23,7 +23,7 @@ class Quotation(models.Model):
     state = fields.Selection(selection=[('draft', 'Draft'),('posted', 'Posted'),('cancel', 'Cancelled'),], string="Status", required=True, readonly=True, copy=False, tracking=True, default="draft")
     document_type = fields.Selection(selection=[('quotation', 'Quotation'),('estimate', 'Estimate'),], string="Type", required=True, store=True, index=True, readonly=True, tracking=True, default="quotation")
     company_id = fields.Many2one(comodel_name='res.company', string='Company', store=True, readonly=True, default=lambda self: self.env.company)
-    company_currency_id = fields.Many2one(string='Company Currency', readonly=True, related='company_id.currency_id')
+    currency_id = fields.Many2one(string='Company Currency', readonly=True, related='company_id.currency_id')
     partner_id = fields.Many2one('res.partner', readonly=True, tracking=True, states={'draft': [('readonly', False)]}, check_company=True, string='Partner', change_default=True)
 
     line_ids = fields.One2many('hdr_sales.quotation.lines', 'quotation_id', string="Quotation Lines")
@@ -31,13 +31,15 @@ class Quotation(models.Model):
     intro = fields.Html(sanitize=True)
     outro = fields.Html(sanitize=True)
 
+    total_amount_untaxed = fields.Monetary(store=True)
+
 class QuotationLine(models.Model):
     _name = 'hdr_sales.quotation.lines'
     _description = 'Sales quotation line'
 
     quotation_id = fields.Many2one("hdr_sales.quotation", string="Quotation", store=True, ondelete="cascade", required=True, index=True)
 
-    currency_id = fields.Many2one(related='quotation_id.company_currency_id', depends=["quotation_id"], store=True, ondelete="restrict")
+    currency_id = fields.Many2one(related='quotation_id.company_currency_id', depends=["quotation_id"], store=True, ondelete="restrict", readonly=True)
 
     name = fields.Char(copy=False, readonly=False)
     description = fields.Html(sanitize=True)
