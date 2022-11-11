6ccf838b069f4e718c46144c9381eb8b7e11dc6c
Dries De Rudder <dries.derudder@gmail.com>
2022-11-11 05:32:44 +0100

    fiscal position

--- hdr_quote/models/quote.py
+++ hdr_quote/models/quote.py
@@ -6,16 +6,21 @@ class QuoteBlock(models.Model):
     _name = 'quote.block'
     _description = 'Quote'
 
-    quote_id = fields.Many2one(comodel_name='quote', string="Quote", store=True)
-    currency_id = fields.Many2one(string='Company Currency', readonly=True, related='quote_id.currency_id')
+    quote_id = fields.Many2one(
+        comodel_name='quote', string="Quote", store=True)
+    currency_id = fields.Many2one(
+        string='Company Currency', readonly=True, related='quote_id.currency_id')
     name = fields.Char(string="Name", copy=True, readonly=False, required=True)
-    description = fields.Html(string="Description", copy=True, readonly=False, sanitize=True)
+    description = fields.Html(string="Description",
+                              copy=True, readonly=False, sanitize=True)
     amount_untaxed = fields.Monetary(string='Untaxed Amount', store=True)
 
     budget_materials = fields.Monetary(string='Budget materials', store=True)
     budget_equipments = fields.Monetary(string='Budget equipments', store=True)
-    budget_production_hours = fields.Float(string='Budget production hours', store=True)
-    budget_installation_hours = fields.Float(string='Budget installation hours', store=True)
+    budget_production_hours = fields.Float(
+        string='Budget production hours', store=True)
+    budget_installation_hours = fields.Float(
+        string='Budget installation hours', store=True)
 
 
 class Quote(models.Model):
@@ -75,26 +80,43 @@ class Quote(models.Model):
         for record in self:
             record.tax_ids = record.company_id.account_sale_tax_id
 
-    name = fields.Char(string='Number', copy=False, compute='_compute_name', readonly=False, store=True, index=True, tracking=True)
-    date = fields.Date(string='Date', required=True, index=True, readonly=True, states={'draft': [('readonly', False)]}, copy=False, tracking=True, default=fields.Date.context_today)
+    name = fields.Char(string='Number', copy=False, compute='_compute_name',
+                       readonly=False, store=True, index=True, tracking=True)
+    date = fields.Date(string='Date', required=True, index=True, readonly=True, states={
+                       'draft': [('readonly', False)]}, copy=False, tracking=True, default=fields.Date.context_today)
     ref = fields.Char(string='Reference', copy=False, tracking=True)
-    state = fields.Selection(selection=[('draft', 'Draft'), ('posted', 'Posted'), ('cancel', 'Cancelled'), ], string='Status', required=True, readonly=True, copy=False, tracking=True, default='draft')
+    state = fields.Selection(selection=[('draft', 'Draft'), ('posted', 'Posted'), ('cancel', 'Cancelled'), ],
+                             string='Status', required=True, readonly=True, copy=False, tracking=True, default='draft')
 
-    quote_type = fields.Selection(selection=[('quote', 'Quote'), ('estimate', 'Estimate')], string='Type', required=True, store=True, readonly=True, tracking=True, default='quote')
-    company_id = fields.Many2one(comodel_name='res.company', string='Company', store=True, readonly=True, default=_get_company_id)
-    currency_id = fields.Many2one(string='Company Currency', readonly=True, related='company_id.currency_id')
+    quote_type = fields.Selection(selection=[('quote', 'Quote'), ('estimate', 'Estimate')],
+                                  string='Type', required=True, store=True, readonly=True, tracking=True, default='quote')
+    company_id = fields.Many2one(comodel_name='res.company', string='Company',
+                                 store=True, readonly=True, default=_get_company_id)
+    currency_id = fields.Many2one(
+        string='Company Currency', readonly=True, related='company_id.currency_id')
 
-    partner_id = fields.Many2one('res.partner', readonly=True, tracking=True, states={'draft': [('readonly', False)]}, check_company=True, string='Partner', change_default=True, ondelete='restrict')
-    tax_ids = fields.Many2many(comodel_name='account.tax',string="Taxes",compute='_compute_tax_ids', store=True, readonly=False, context={'active_test': False})
-    # country_code = fields.Char(related='company_id.account_fiscal_country_id.code', readonly=True)
-    # user_id = fields.Many2one(string='User', related='invoice_user_id', help='Technical field used to fit the generic behavior in mail templates.')
+    partner_id = fields.Many2one('res.partner', readonly=True, tracking=True, states={'draft': [(
+        'readonly', False)]}, check_company=True, string='Partner', change_default=True, ondelete='restrict')
+    fiscal_position_id = fields.Many2one('account.fiscal.position', string='Fiscal Position',
+                                         readonly=True, domain="[('company_id', '=', company_id)]", ondelete="restrict")
+    tax_ids = fields.Many2many(comodel_name='account.tax', string="Taxes",
+                               compute='_compute_tax_ids', store=True, readonly=False, context={'active_test': False})
 
     introduction = fields.Html(string="Introduction", sanitize=True)
     conditions = fields.Html(string="Conditions", sanitize=True)
 
-    block_ids = fields.One2many(comodel_name="quote.block", inverse_name="quote_id", string="Blocks", copy=True)
+    block_ids = fields.One2many(
+        comodel_name="quote.block", inverse_name="quote_id", string="Blocks", copy=True)
 
     # === Amount fields ===
-    amount_untaxed = fields.Monetary(string='Untaxed Amount', store=True, readonly=True, tracking=True, compute='_compute_amount')
-    amount_tax = fields.Monetary(string='Tax', store=True, readonly=True, compute='_compute_amount')
-    amount_total = fields.Monetary(string='Total', store=True, readonly=True, compute='_compute_amount')
+    amount_untaxed = fields.Monetary(
+        string='Untaxed Amount', store=True, readonly=True, tracking=True, compute='_compute_amount')
+    amount_tax = fields.Monetary(
+        string='Tax', store=True, readonly=True, compute='_compute_amount')
+    amount_total = fields.Monetary(
+        string='Total', store=True, readonly=True, compute='_compute_amount')
+
+    @api.onchange('partner_id')
+    def _onchange_partner(self):
+        self.fiscal_position_id = self.env['account.fiscal.position'].get_fiscal_position(self.partner_id.id)
+        
