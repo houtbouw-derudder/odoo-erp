3972e5676052b2533e227c925f76e6315aa8cf66
Dries De Rudder <dries.derudder@gmail.com>
2023-06-08 22:03:55 +0200

    Revert "Revert "Revert "Merge branch 'main' into staging"""
    This reverts commit 432eec609df1e5ce6f70dded416e98c7c03dbc5c.

---
+++ geodynamics/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Geodynamics',
+    'version': '2.2',
+    'category': 'Customization',
+    'author': 'Houtbouw De Rudder bv',
+    'description': """
+        Integration with geodynamics
+    """,
+    'depends': [
+        'project',
+        'hr_timesheet'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/geodynamics_views.xml',
+        'views/res_config_settings_views.xml',
+        'views/geodynamics_reporting_views.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'license': 'OPL-1'
+}
