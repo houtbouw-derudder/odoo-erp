dd12dbda1be67f804467e9f3e686db80aff289ee
Dries De Rudder <dries.derudder@gmail.com>
2024-11-20 21:44:40 +0100

    issue with payment terms

--- hdr_quote/models/quote.py
+++ hdr_quote/models/quote.py
@@ -103,6 +103,7 @@ class Quote(models.Model):
                     untaxed_amount_currency=1,
                     sign=1)
                 
+                print(terms)
                 record.date_due = terms[0].get('date', record.date) if len(terms) > 0 else record.date
             else:
                 record.date_due = record.date
