e8540b25f90405f388927e3f97243e4c79c85c60
Dries De Rudder <dries.derudder@gmail.com>
2024-12-03 14:44:01 +0100

    remove modules hdr_aankoop, geodynamics and report customization

--- geodynamics/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import geodynamics_postcalculation
-from . import res_config_settings
-from . import geodynamics_api
-from . import analytic_account
--- geodynamics/models/analytic_account.py
+++
@@ -1,7 +0,0 @@
-
-from odoo import fields, models
-
-class AccountAnalyticLine(models.Model):
-    _inherit = 'account.analytic.line'
-
-    postcalculation_line_id = fields.Many2one('geodynamics.postcalculation.line', 'Postcalculation Line', required=False, ondelete='cascade', index=True)
